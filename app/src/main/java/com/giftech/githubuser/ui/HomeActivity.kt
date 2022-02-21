eb866ee065635ffa15395e2b9250029c82f448cf
galihif <galihindra2401@mail.ugm.ac.id>
2022-02-21 15:38:08 +0700

    rename HomeActivity.kt

--- app/src/main/java/com/giftech/githubuser/ui/HomeActivity.kt
+++ app/src/main/java/com/giftech/githubuser/ui/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.giftech.githubuser
+package com.giftech.githubuser.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.giftech.githubuser.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
