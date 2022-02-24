0abbdbd8f4f9dfb1b298ae7e77e9b9d8bb71d3d7
galihif <galihindra2401@mail.ugm.ac.id>
2022-02-24 11:08:19 +0700

    refactor: use loadCircleImage function

--- app/src/main/java/com/giftech/githubuser/ui/DetailActivity.kt
+++ app/src/main/java/com/giftech/githubuser/ui/DetailActivity.kt
@@ -2,10 +2,10 @@ package com.giftech.githubuser.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
 import com.giftech.githubuser.R
 import com.giftech.githubuser.data.User
 import com.giftech.githubuser.databinding.ActivityDetailBinding
+import com.giftech.githubuser.utils.AppUtils.loadCircleImage
 
 class DetailActivity : AppCompatActivity() {
 
@@ -20,12 +20,7 @@ class DetailActivity : AppCompatActivity() {
             val user = extras.getParcelable<User>(USER_DATA)
 
             with(binding){
-                Glide
-                    .with(this@DetailActivity)
-                    .load(user?.avatar)
-                    .centerCrop()
-                    .circleCrop()
-                    .into(header.ivAvatar)
+                header.ivAvatar.loadCircleImage(user?.avatar)
 
                 header.ivName.text = user?.name
                 header.ivUsername.text = user?.username
