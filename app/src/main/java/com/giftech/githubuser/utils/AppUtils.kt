767a6d9fcab9e933dfb2eb70d74313a1c47c0e7d
galihif <galihindra2401@mail.ugm.ac.id>
2022-02-25 19:57:50 +0700

    feat: add function to show toast

--- app/src/main/java/com/giftech/githubuser/utils/AppUtils.kt
+++ app/src/main/java/com/giftech/githubuser/utils/AppUtils.kt
@@ -1,6 +1,8 @@
 package com.giftech.githubuser.utils
 
+import android.content.Context
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.giftech.githubuser.R
@@ -14,6 +16,10 @@ object AppUtils {
             .circleCrop()
             .apply(RequestOptions.placeholderOf(R.drawable.ic_loading).error(R.drawable.ic_error))
             .into(this)
+    }
+
+    fun showToast(context: Context, message:String){
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 
 }
