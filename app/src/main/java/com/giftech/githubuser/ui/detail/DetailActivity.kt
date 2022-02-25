5fa8c5d83357eacfa2386d7e58e5cbcff3220570
galihif <galihindra2401@mail.ugm.ac.id>
2022-02-25 19:57:28 +0700

    feat: add feature to show detail user from API

--- app/src/main/java/com/giftech/githubuser/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/giftech/githubuser/ui/detail/DetailActivity.kt
@@ -1,19 +1,26 @@
 package com.giftech.githubuser.ui.detail
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.giftech.githubuser.R
 import com.giftech.githubuser.data.User
 import com.giftech.githubuser.databinding.ActivityDetailBinding
 import com.giftech.githubuser.utils.AppUtils.loadCircleImage
+import com.giftech.githubuser.viewmodel.ViewModelFactory
 
 class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
+    private lateinit var viewModel: DetailViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val factory = ViewModelFactory.getInstance()
+        viewModel = ViewModelProvider(this,factory)[DetailViewModel::class.java]
 
         val extras = intent.extras
         if(extras != null){
@@ -21,18 +28,42 @@ class DetailActivity : AppCompatActivity() {
 
             supportActionBar?.title = user?.username
 
-            with(binding){
-                header.ivAvatar.loadCircleImage(user?.avatar)
+            viewModel.getDetailUser(user?.username!!).observe(this, {
+                populateView(it)
+            })
 
-                header.ivName.text = user?.name
-                header.ivUsername.text = user?.username
+            viewModel.loading.observe(this, {loading ->
+                if(loading){
+                    showLoading(true)
+                } else {
+                    showLoading(false)
+                }
+            })
+        }
+    }
 
-                desc.tvCompany.text = user?.company
-                desc.tvLocation.text = user?.location
-                desc.tvRepository.text = getString(R.string.format_repository, user?.repository)
-                desc.tvFollow.text = getString(R.string.format_follow, user?.followers, user?.following)
+    private fun populateView(user: User) {
+        with(binding){
+            header.ivAvatar.loadCircleImage(user.avatar)
 
-            }
+            header.ivName.text = user.name
+            header.ivUsername.text = user.username
+
+            desc.tvCompany.text = user.company
+            desc.tvLocation.text = user.location
+            desc.tvRepository.text = getString(R.string.format_repository, user.repository)
+            desc.tvFollow.text = getString(R.string.format_follow, user.followers, user.following)
+
+        }
+    }
+
+    private fun showLoading(loading: Boolean) {
+        if(loading){
+            binding.loading.root.visibility = View.VISIBLE
+            binding.sectionProfile.visibility = View.INVISIBLE
+        } else {
+            binding.loading.root.visibility = View.INVISIBLE
+            binding.sectionProfile.visibility = View.VISIBLE
         }
     }
 
