3f137eaec644253ff637e42e85258f1e1e1b7124
galihif <galihindra2401@mail.ugm.ac.id>
2022-03-03 11:04:50 +0700

    feat: get if saved data is true or false for dark theme and change the theme

--- app/src/main/java/com/giftech/githubuser/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/giftech/githubuser/ui/splash/SplashActivity.kt
@@ -5,14 +5,22 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
 import com.giftech.githubuser.R
 import com.giftech.githubuser.ui.home.HomeActivity
+import com.giftech.githubuser.viewmodel.ViewModelFactory
 
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val factory = ViewModelFactory.getInstance(this)
+        val viewModel = ViewModelProvider(this,factory)[SplashViewModel::class.java]
+
+        setTheme(viewModel.getIsDarkTheme())
 
         supportActionBar?.hide()
 
@@ -23,5 +31,13 @@ class SplashActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }, delayTime)
+    }
+
+    private fun setTheme(isDark: Boolean) {
+        if (isDark) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
