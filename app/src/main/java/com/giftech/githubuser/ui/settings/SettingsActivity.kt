96f71f34dc73bfb6356aa8b2110763c31385e59d
galihif <galihindra2401@mail.ugm.ac.id>
2022-03-03 11:05:11 +0700

    feat: can set dark theme to local data

--- app/src/main/java/com/giftech/githubuser/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/giftech/githubuser/ui/settings/SettingsActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
 import com.giftech.githubuser.databinding.ActivitySettingsBinding
+import com.giftech.githubuser.viewmodel.ViewModelFactory
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -14,14 +16,20 @@ class SettingsActivity : AppCompatActivity() {
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val factory = ViewModelFactory.getInstance(this)
+        val viewModel = ViewModelProvider(this,factory)[SettingsViewModel::class.java]
+
+        binding.switchTheme.isChecked = viewModel.getIsDarkTheme()
 
         binding.switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
             if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
                 binding.switchTheme.isChecked = true
+                viewModel.setDarkTheme(true)
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
                 binding.switchTheme.isChecked = false
+                viewModel.setDarkTheme(false)
             }
         }
     }
